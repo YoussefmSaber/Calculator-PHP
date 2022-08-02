ab4d79d5f02c2cda9a5bd7fbf7228840b34c9ef6
Youssef Mohammed <youssef.mu.saber@gmail.com>
2022-08-02 22:09:02 +0200

    Change the style of the calculater with simpler version

--- index.php
+++ index.php
@@ -1,46 +1,22 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculator using PHP</title>
+    <title>Document</title>
 </head>
 <body>
-
-    <table>
-        <tr>
-            <td colspan="4"><input type="text"/></td>
-        </tr>
-        <tr>
-            <td><input type="submit" name="submit" value="7"/></td>
-            <td><input type="submit" name="submit" value="8"/></td>
-            <td><input type="submit" name="submit" value="9"/></td>
-            <td><input type="submit" name="op" value="÷"/></td>
-        </tr>
-        <tr>
-            <td><input type="submit" name="submit" value="4"/></td>
-            <td><input type="submit" name="submit" value="5"/></td>
-            <td><input type="submit" name="submit" value="6"/></td>
-            <td><input type="submit" name="op" value="*"/></td>
-        </tr>
-        <tr>
-            <td><input type="submit" name="submit" value="1"/></td>
-            <td><input type="submit" name="submit" value="2"/></td>
-            <td><input type="submit" name="submit" value="3"/></td>
-            <td><input type="submit" name="op" value="+"/></td>
-        </tr>
-        <tr>
-            <td><input type="submit" name="submit" value="0"/></td>
-            <td><input type="submit" name="submit" value="C"/></td>
-            <td><input type="submit" name="equals" value="="/></td>
-            <td><input type="submit" name="op" value="-"/></td>
-        </tr>
-    </table>
-
+    <form action="calculate.php" method="post">
+        <input type="text" name="num1">
+        <input type="text" name="num2">
+        <select name="op">
+            <option value="add">Addition</option>
+            <option value="sub">Subtract</option>
+            <option value="mul">Multiply</option>
+            <option value="div">Divide</option>
+        </select>
+        <input type="submit" name="calculate">
+    </form>
 </body>
 </html>
