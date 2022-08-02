7c7c04c4dc069e2ae1831452177734b8215d2eef
Youssef Mohammed <youssef.mu.saber@gmail.com>
2022-08-03 00:11:00 +0200

    Fixed some php bugs and added css file for better look.

--- index.php
+++ index.php
@@ -4,58 +4,77 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Calculator</title>
+    
 </head>
 <body>
-    <form method="post">
-        <input type="text" name="num1">
-        <input type="text" name="num2">
-        <select name="op">
-            <option value="add">Addition</option>
-            <option value="sub">Subtract</option>
-            <option value="mul">Multiply</option>
-            <option value="div">Divide</option>
-        </select>
-        <input type="submit" name="calculate">
-    </form>
-    <?php
+    <div class="container">
+    
+    
+        <form method="post" class="form">
+            <input type="text" name="num1" placeholder="Number1" class="num-input brdr-rad">
+            <input type="text" name="num2" placeholder="Number2" class="num-input brdr-rad">
 
-    if(isset($_POST['calculate']))
-    {
-        $num1 = $_POST['num1'];
-        $num2 = $_POST['num2'];
-        $operator = $_POST['op'];
-        $outputcase = 1;
+            <div class="select">
+                <select name="op" style="width:170px" class="brdr-rad">
+                    <option value="none">None</option>
+                    <option value="add">Addition</option>
+                    <option value="sub">Subtract</option>
+                    <option value="mul">Multiply</option>
+                    <option value="div">Divide</option>
+                </select>
+            </div>
 
-        switch($operator)
+            <div class="select">
+                <button type="submit" name="calculate" value="Calculate" class="brdr-rad btn">Calculate</button>
+            </div>
+        
+        </form>
+        <div class="result">
+        <?php
+
+        if(isset($_POST['calculate']))
         {
-            case 'add':
-                $result = $num1 + $num2;
-                break;
-            case 'sub':
-                $result = $num1 - $num2;
-                break;
-            case 'mul':
-                $result = $num1 * $num2;
-                break;
-            case 'div':
-                if($num2 == 0)
-                {
+            $num1 = $_POST['num1'];
+            $num2 = $_POST['num2'];
+            $operator = $_POST['op'];
+            $outputcase = 1;
+
+            switch($operator)
+            {
+                case 'none':
                     $outputcase = 2;
-                    echo "Can't divide by 0";
-                }
-                else
-                {
-                    $reslut = $num1 / $num2;
-                }
-                break;
+                    echo "Please select an operation";
+                    break;
+                case 'add':
+                    $result = $num1 + $num2;
+                    break;
+                case 'sub':
+                    $result = $num1 - $num2;
+                    break;
+                case 'mul':
+                    $result = $num1 * $num2;
+                    break;
+                case 'div':
+                    if($num2 == 0)
+                    {
+                        $outputcase = 2;
+                        echo "Can't divide by 0";
+                    }
+                    else
+                    {
+                        $reslut = $num1 / $num2;
+                    }
+                    break;
+            }
+            if($outputcase == 1)
+            {
+                print $result;
+            }
         }
-        if($outputcase == 1)
-        {
-            echo "The answer is: $result";
-        }
-    }
-
-?>
+        ?>
+</div>
+</div>
 </body>
 </html>
