488b7cf88f83ec375a377c1c9c23d277d15e8c1f
Youssef Mohammed <youssef.mu.saber@gmail.com>
2022-08-02 22:23:57 +0200

    Add some php script for the calculater. Added the functionality for add, mul, div and sub the numbers

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="calculate.php" method="post">
+    <form method="post">
         <input type="text" name="num1">
         <input type="text" name="num2">
         <select name="op">
@@ -18,5 +18,44 @@
         </select>
         <input type="submit" name="calculate">
     </form>
+    <?php
+
+    if(isset($_POST['calculate']))
+    {
+        $num1 = $_POST['num1'];
+        $num2 = $_POST['num2'];
+        $operator = $_POST['op'];
+        $outputcase = 1;
+
+        switch($operator)
+        {
+            case 'add':
+                $result = $num1 + $num2;
+                break;
+            case 'sub':
+                $result = $num1 - $num2;
+                break;
+            case 'mul':
+                $result = $num1 * $num2;
+                break;
+            case 'div':
+                if($num2 == 0)
+                {
+                    $outputcase = 2;
+                    echo "Can't divide by 0";
+                }
+                else
+                {
+                    $reslut = $num1 / $num2;
+                }
+                break;
+        }
+        if($outputcase == 1)
+        {
+            echo "The answer is: $result";
+        }
+    }
+
+?>
 </body>
 </html>
