43dbff26f8e2543d1b1f15ccae42ff7c247623f6
Youssef Mohammed <youssef.mu.saber@gmail.com>
2022-08-02 20:37:24 +0200

    Created the basic ui for the calculater using html in the php file

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculator using PHP</title>
+</head>
+<body>
+
+    <table>
+        <tr>
+            <td colspan="4"><input type="text"/></td>
+        </tr>
+        <tr>
+            <td><input type="submit" name="submit" value="7"/></td>
+            <td><input type="submit" name="submit" value="8"/></td>
+            <td><input type="submit" name="submit" value="9"/></td>
+            <td><input type="submit" name="op" value="÷"/></td>
+        </tr>
+        <tr>
+            <td><input type="submit" name="submit" value="4"/></td>
+            <td><input type="submit" name="submit" value="5"/></td>
+            <td><input type="submit" name="submit" value="6"/></td>
+            <td><input type="submit" name="op" value="*"/></td>
+        </tr>
+        <tr>
+            <td><input type="submit" name="submit" value="1"/></td>
+            <td><input type="submit" name="submit" value="2"/></td>
+            <td><input type="submit" name="submit" value="3"/></td>
+            <td><input type="submit" name="op" value="+"/></td>
+        </tr>
+        <tr>
+            <td><input type="submit" name="submit" value="0"/></td>
+            <td><input type="submit" name="submit" value="C"/></td>
+            <td><input type="submit" name="equals" value="="/></td>
+            <td><input type="submit" name="op" value="-"/></td>
+        </tr>
+    </table>
+
+</body>
+</html>
